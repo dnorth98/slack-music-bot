7f42114aede2eb295569f38f4d328a5af21e8d38
Dave North <dnorth98@gmail.com>
2015-03-15 21:01:59 +0000

    more escaping

--- web/index.php
+++ web/index.php
@@ -127,11 +127,11 @@ function help($app,$slackUser,$text)
 
   	$app['monolog']->addDebug('HELP routine: ' . $text );
 
-	$helpText = 'You can ask me the following:\\n';
-	$helpText = $helpText . 'play - play whatever is queued up\\n';
-	$helpText = $helpText . 'stop - stop the funky beats\\n';
-	$helpText = $helpText . 'nowplaying - report back what is currently playing\\n';
-	$helpText = $helpText . 'request <songname> by <artist> - request a song be added to the playlist\\n';
+	$helpText = 'You can ask me the following:\\\n';
+	$helpText = $helpText . 'play - play whatever is queued up\\\n';
+	$helpText = $helpText . 'stop - stop the funky beats\\\n';
+	$helpText = $helpText . 'nowplaying - report back what is currently playing\\\n';
+	$helpText = $helpText . 'request <songname> by <artist> - request a song be added to the playlist\\\n';
 
 	$returnArray['text'] = $helpText;
 
