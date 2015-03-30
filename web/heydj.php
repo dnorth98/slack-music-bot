dde527431c0818734faaf2821d558580c33c7abe
Dave North <dnorth98@gmail.com>
2015-03-30 00:46:45 +0000

    added nextup handler

--- web/heydj.php
+++ web/heydj.php
@@ -151,6 +151,29 @@ function nowplaying($app,$slackUser,$text)
 	return $returnJSON;
 }
 
+function nextup($app,$slackUser,$text) 
+{
+	$returnArray = array("text" => "");
+
+  	$app['monolog']->addDebug('HEYDJ NEXTUP routine: ' . $slackUser . ' ' . $text );
+
+	// write the command to the DB
+	$status = writeToDB($app,$slackUser,"nowplaying","");
+	if ($status)
+	{
+		$returnArray['text'] = 'OK, ' . $slackUser . ' I\'ve asked the music controller to tell us what\'s spinning next';
+	} else
+	{
+		$returnArray['text'] = 'I\'m sorry ' . $slackUser . ', I was unable to ask the music controller to tell us what\s up next';
+	}
+
+	$returnJSON = json_encode($returnArray);	
+
+  	$app['monolog']->addDebug('HEYDJ NEXTUP routine returning: ' . $returnJSON );
+
+	return $returnJSON;
+}
+
 function help($app,$slackUser,$text)
 {
 	$returnArray = array("text" => "");
